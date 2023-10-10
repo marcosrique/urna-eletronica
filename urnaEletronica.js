960b5ac5faeb614ae50a5c458af1fd8994afa84f
marcosrique <marcosrique@gmail.com>
2023-10-10 16:54:09 -0300

    Feat: início da codificação da urna eletrônica

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -26,7 +26,7 @@ if (opcao ===
     console.log('Nenhuma das opções');
 }
 //Quando é só uma instrução o if e o else não precisa de chave
-*/
+
 
 // Estrutura de repetição: do-while (faça-enquanto)
 
@@ -51,4 +51,87 @@ while (opcao !==0) {
     console.log('Instrução 5');
     console.log('Instrução 6');
 }
+
+
+let candidato_1 = 0;
+let candidato_2 = 0;
+let candidato_3 = 0;
+let voto_branco = 0;
+let voto_nulo = 0;
+
+
+function votarEleic() {
+
+     
+
+    while (x != 0) {
+        
+        x = parseInt(getElementById('x').value);
+
+        switch (x) {
+    
+         case 1:
+            candidato_1 ++;
+         break;
+         
+         case 2:
+            candidato_2 ++;
+        break;
+    
+        case 3:
+            candidato_3 ++;
+        break;
+    
+        case 5:
+            voto_branco ++;
+        break;
+    
+        case 8:
+            voto_nulo ++;
+        break;
+        
+           
+        }
+        
+    }
+    document.getElementById('resultado').innerHTML += 'O Candidato 1 = ' + candidato_1 + 'votos'; 
+
+}
 */
+
+
+
+
+
+function urnaEletronica() {
+    
+    
+    // Aqui vai todo o código do programa....
+    
+    console.log('Iniciando o programa');
+    
+ let
+    totalVotosCandidato1 = 0,
+    totalVotosCandidato2 = 0,
+    totalVotosCandidato3 = 0,
+    totalVotosEmBranco = 0,
+    totalVotosNulo = 0,
+    x = 0
+    
+    do {
+
+
+        x = parseInt(document.getElementById('x').innerHTML);
+
+        switch (x) {
+            case 1:
+                totalVotosCandidato1 = x;
+            break;
+        }
+
+    } while (x !=0 );
+
+    document.getElementById('resultado').innerHTML =+ 'O Candidato 1 = ' + totalVotosCandidato1 + 'votos'; 
+
+}
+
